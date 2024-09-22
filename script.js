a65d57b8178a0217dfa26b56dae3e3c415c328fd
andreaDevelope <andrea_develope@outlook.it>
2024-09-22 19:41:03 +0200

    refactoring-scroll-header

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hero = document.querySelector(".hero");
 const buttonheader = document.querySelector(".nav-button");
 
 function haderScroll() {
-  if (window.scrollY > hero.offsetHeight) {
+  if (window.scrollY > hero.offsetHeight - 100) {
     header.setAttribute("id", "scroll");
     buttonheader.setAttribute("id", "nav-button2");
   } else {
@@ -11,23 +11,5 @@ function haderScroll() {
     buttonheader.removeAttribute("id");
   }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const textGroup = document.getElementById("textGroup");
-
-  function animationM() {
-    if (textGroup.classList.contains("charat-entra")) {
-      textGroup.classList.remove("charat-entra");
-      textGroup.classList.add("charat-esce");
-    } else {
-      textGroup.classList.remove("charat-esce");
-      textGroup.classList.add("charat-entra");
-    }
-
-    setTimeout(toggleAnimation, Math.random() * 1000 + 1000);
-  }
-
-  animationM();
-});
 
 window.addEventListener("scroll", haderScroll);
